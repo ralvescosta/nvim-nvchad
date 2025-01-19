b5c69ff68f79cc73cbcb1ca7f272e061238c22fb
ralvescosta <rafael.rac.mg@gmail.com>
2025-01-19 16:07:13 -0300

    terraform language server

--- lua/configs/mason-lspconfig.lua
+++ lua/configs/mason-lspconfig.lua
@@ -1,11 +1,13 @@
 require("mason-lspconfig").setup({
   ensure_installed = {
     "rust-analyzer",
-    "codelldb",
     "gopls",
     "delve",
     "gofumpt",
     "goimports-reviser",
     "golines",
+    "terraform-ls",
+    "lua-language-server",
+    "codelldb"
   }
 })
