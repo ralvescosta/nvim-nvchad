cf2f5575e4371b25e131d157a106eeb833952740
ralvescosta <rafael.costa@asapcard.com.br>
2025-01-27 10:00:34 -0300

    feat:

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -22,6 +22,16 @@ return {
         "json", "terraform", "dockerfile", "yaml"
       },
     },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = {
+        "vim", "lua", "vimdoc", "rust",
+        "go", "gomod", "gosum", "gowork",
+        "c", "markdown", "terraform", "bash",
+        "toml", "proto", "sql", "graphql", "cmake",
+        "json", "terraform", "dockerfile", "yaml",
+      }
+    }
   },
 
   {
