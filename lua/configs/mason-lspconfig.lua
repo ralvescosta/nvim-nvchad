7efe7d459abd07e19dc5e4d64bdffbb7e89bd45d
ralvescosta <rafael.rac.mg@gmail.com>
2025-02-09 14:58:08 -0300

    feat: none ls with cspell

--- lua/configs/mason-lspconfig.lua
+++ lua/configs/mason-lspconfig.lua
@@ -1,14 +1,18 @@
-require("mason-lspconfig").setup({
-  ensure_installed = {
-    "rust-analyzer",
-    "gopls",
-    "delve",
-    "gofumpt",
-    "goimports-reviser",
-    "golines",
-    "terraform-ls",
-    "lua-language-server",
-    "codelldb",
-    "cspell",
-  }
-})
+require("mason-lspconfig").setup {
+  opts = {
+    ensure_installed = {
+      "rust-analyzer",
+      "gopls",
+      "delve",
+      "gofumpt",
+      "goimports-reviser",
+      "golines",
+      "terraform-ls",
+      "lua-language-server",
+      "codelldb",
+      "cspell",
+      "codespell",
+      "misspell",
+    },
+  },
+}
---
+++ lua/plugins/none-ls.nvim.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "davidmh/cspell.nvim",
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    depends = { "davidmh/cspell.nvim" },
+    opts = function(_, opts)
+      local cspell = require "cspell"
+      opts.sources = opts.sources or {}
+
+      table.insert(
+        opts.sources,
+        cspell.diagnostics.with {
+          diagnostics_postprocess = function(diagnostic)
+            diagnostic.severity = vim.diagnostic.severity.HINT
+          end,
+        }
+      )
+
+      table.insert(
+        opts.sources,
+        cspell.code_actions.with {
+          -- Adding a name to help with debugging
+          -- hilo
+          name = "cspell_code_actions",
+        }
+      )
+    end,
+  },
+}
