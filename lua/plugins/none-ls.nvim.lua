f7cccd2b4dc8ca3c6b6897f1b87dd12f801b39c8
ralvescosta <rafael.costa@asapcard.com.br>
2025-02-13 16:21:39 -0300

    feat: comment null ls, new words

--- lua/plugins/none-ls.nvim.lua
+++ lua/plugins/none-ls.nvim.lua
@@ -1,28 +1,28 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    event = "VeryLazy",
-    depends = { "davidmh/cspell.nvim" },
-    opts = function(_, opts)
-      local cspell = require "cspell"
-      opts.sources = opts.sources or {}
-
-      table.insert(
-        opts.sources,
-        cspell.diagnostics.with {
-          diagnostics_postprocess = function(diagnostic)
-            diagnostic.severity = vim.diagnostic.severity.HINT
-          end,
-        }
-      )
-
-      table.insert(
-        opts.sources,
-        cspell.code_actions.with {
-          -- Adding a name to help with debugging
-          name = "cspell_code_actions",
-        }
-      )
-    end,
-  },
+  -- {
+  --   "nvimtools/none-ls.nvim",
+  --   event = "VeryLazy",
+  --   depends = { "davidmh/cspell.nvim" },
+  --   opts = function(_, opts)
+  --     local cspell = require "cspell"
+  --     opts.sources = opts.sources or {}
+  --
+  --     table.insert(
+  --       opts.sources,
+  --       cspell.diagnostics.with {
+  --         diagnostics_postprocess = function(diagnostic)
+  --           diagnostic.severity = vim.diagnostic.severity.HINT
+  --         end,
+  --       }
+  --     )
+  --
+  --     table.insert(
+  --       opts.sources,
+  --       cspell.code_actions.with {
+  --         -- Adding a name to help with debugging
+  --         name = "cspell_code_actions",
+  --       }
+  --     )
+  --   end,
+  -- },
 }
