e11dc8a5836b5524f7b9fbc0eefd34d5aedb835d
ralvescosta <rafael.rac.mg@gmail.com>
2025-02-16 10:03:00 -0300

    feat: nvim lint

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,15 +1,30 @@
 return {
-  "mfussenegger/nvim-lint",
-  config = function()
-    require("lint").linters_by_ft = {
-      go = { "cspell" },
-    }
+  {
+    "mfussenegger/nvim-lint",
+    event = "VeryLazy",
+    config = function()
+      local lint = require "lint"
 
-    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-      callback = function()
-        require("lint").try_lint()
-        require("lint").try_lint "cspell"
-      end,
-    })
-  end,
+      lint.linters_by_ft = {
+        go = { "cspell" },
+        rust = { "cspell" },
+        lua = { "cspell" },
+        typescript = { "cspell" },
+        javascript = { "cspell" },
+        python = { "cspell" },
+        java = { "cspell" },
+        terraform = { "cspell" },
+        text = { "cspell" },
+        markdown = { "cspell" },
+      }
+
+      -- 🔥 Debugging: Show messages in `:messages`
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        callback = function()
+          vim.notify("Running CSpell Linter...", vim.log.levels.INFO, { timeout = 100 })
+          require("lint").try_lint()
+        end,
+      })
+    end,
+  },
 }
