2055febaaf69e40e8c56e401ea0e1101ceb54b2d
ralvescosta <rafael.rac.mg@gmail.com>
2025-02-16 17:44:10 -0300

    feat: nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,19 +2,92 @@ return {
   {
     "nvim-tree/nvim-tree.lua",
     opts = {
-      git = {
-        enable = true, -- Ensure git integration is enabled
-        ignore = false, -- Show files that are in .gitignore
+      disable_netrw = true,
+      hijack_netrw = true,
+      open_on_setup = false,
+      ignore_ft_on_setup = {
+        "startify",
+        "dashboard",
+        "alpha",
       },
-      filters = {
-        git_ignored = false,
+      open_on_tab = false,
+      hijack_cursor = false,
+      update_cwd = true,
+      hijack_directories = {
+        enable = true,
+        auto_open = true,
       },
-      renderer = {
-        highlight_git = true,
-        highlight_diagnostics = true,
+      diagnostics = {
+        enable = true,
+        icons = {
+          hint = "",
+          info = "",
+          warning = "",
+          error = "",
+        },
       },
       update_focused_file = {
         enable = true,
+        update_cwd = true,
+        ignore_list = {},
+      },
+      git = {
+        enable = true,
+        ignore = true,
+        timeout = 500,
+      },
+      view = {
+        width = 30,
+        height = 30,
+        hide_root_folder = false,
+        side = "left",
+        auto_resize = true,
+        mappings = {
+          custom_only = false,
+          -- list = {
+          --   { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+          --   { key = "h", cb = tree_cb "close_node" },
+          --   { key = "v", cb = tree_cb "vsplit" },
+          -- },
+        },
+        number = false,
+        relativenumber = false,
+      },
+      actions = {
+        quit_on_open = true,
+        window_picker = { enable = true },
+      },
+      renderer = {
+        highlight_git = true,
+        root_folder_modifier = ":t",
+        icons = {
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = true,
+          },
+          glyphs = {
+            default = "",
+            symlink = "",
+            git = {
+              unstaged = "",
+              staged = "S",
+              unmerged = "",
+              renamed = "➜",
+              deleted = "",
+              untracked = "U",
+              ignored = "◌",
+            },
+            folder = {
+              default = "",
+              open = "",
+              empty = "",
+              empty_open = "",
+              symlink = "",
+            },
+          },
+        },
       },
     },
   },
