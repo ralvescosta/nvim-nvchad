e6d82bbff68e71235e0923e22e85889ad36267d0
ralvescosta <rafael.rac.mg@gmail.com>
2025-02-16 10:03:11 -0300

    feat: lsp auto format

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,16 +1,34 @@
--- load defaults i.e lua_lsp
+-- Load NvChad's default LSP config
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local servers = { "misspell", "cspell",  "gopls", "terraformls", "lua_ls", "yamlls", "dockerls", "docker-compose_langserver", "helm_ls", "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
+-- List of LSP servers
+local servers = { "gopls", "terraformls", "lua_ls", "yamlls", "dockerls", "helm_ls", "html", "cssls" }
+
+-- Define a function to enable formatting on save
+local function lsp_format_on_save(client, bufnr)
+  if client.supports_method "textDocument/formatting" then
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format { async = false }
+      end,
+    })
+  end
+end
+
+-- Setup LSP servers with auto-format on save
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
+    on_attach = function(client, bufnr)
+      -- Attach default NvChad LSP settings
+      nvlsp.on_attach(client, bufnr)
+
+      -- Enable auto-format on save
+      lsp_format_on_save(client, bufnr)
+    end,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
   }
