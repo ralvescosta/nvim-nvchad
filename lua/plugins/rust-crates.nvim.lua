fec2f7aa31526e0c225a01a66cc9ee3e915f4224
ralvescosta <rafael.rac.mg@gmail.com>
2025-02-18 08:45:16 -0300

    feat: disabling rust crates

--- lua/plugins/rust-crates.nvim.lua
+++ lua/plugins/rust-crates.nvim.lua
@@ -1,20 +1,11 @@
 return {
-  {
-    "saecki/crates.nvim",
-    ft = { "toml" },
-    event = { "BufRead Cargo.toml" },
-    tag = 'stable',
-    config = function()
-      require("crates").setup {
-        completion = {
-          cmp = {
-            enabled = true
-          },
-        },
-      }
-      require("cmp").setup.buffer({
-        sources = { { name = "crates" } }
-      })
-    end
-  }
+  -- {
+  --   "saecki/crates.nvim",
+  --   ft = { "toml" },
+  --   event = { "BufRead Cargo.toml" },
+  --   tag = 'stable',
+  --   config = function()
+  --     require("crates").setup()
+  --   end
+  -- }
 }
