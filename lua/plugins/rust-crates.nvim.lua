83810c54e920e996750c7e3804045cd49d79ffc2
ralvescosta <rafael.rac.mg@gmail.com>
2025-02-20 08:36:14 -0300

    feat: update rust-crates

--- lua/plugins/rust-crates.nvim.lua
+++ lua/plugins/rust-crates.nvim.lua
@@ -1,11 +1,18 @@
 return {
-  -- {
-  --   "saecki/crates.nvim",
-  --   ft = { "toml" },
-  --   event = { "BufRead Cargo.toml" },
-  --   tag = 'stable',
-  --   config = function()
-  --     require("crates").setup()
-  --   end
-  -- }
+  {
+    "saecki/crates.nvim",
+    ft = { "toml" },
+    config = function()
+      require("crates").setup {
+        completion = {
+          cmp = {
+            enabled = true
+          },
+        },
+      }
+      require("cmp").setup.buffer({
+        sources = { { name = "crates" } }
+      })
+    end
+  }
 }
